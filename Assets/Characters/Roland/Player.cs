6ecc603610dd312669bece753b77f263da0aad7b
Joshua Norvelle <joshua.norvelle@gmail.com>
2022-05-30 13:39:45 -0500

    Pause Menu
    Updated pause menu to complete game loop

--- Assets/Characters/Roland/Player.cs
+++ Assets/Characters/Roland/Player.cs
@@ -73,7 +73,6 @@ public class Player : MonoBehaviour
 
   void Defeated()
   {
-    Debug.Log("Player Ded");
     gm.PauseGame();
   }
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
   [SerializeField] int score = 0;
+  [SerializeField] GameObject pauseMenu;
 
   public bool isPaused = false;
 
@@ -25,5 +27,18 @@ public class GameManager : MonoBehaviour
   {
     isPaused = true;
     Time.timeScale = 0f;
+    pauseMenu.SetActive(true);
+  }
+
+  public void RestartGame()
+  {
+    Time.timeScale = 1f;
+    isPaused = false;
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+  }
+
+  public void QuitGame()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
   }
 }
